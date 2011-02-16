6acb0a2206c4b7ec446c87b2925ff82d63f2893d
Maurizio Lupo <mlupo@mlupo-laptop.(none)>
2011-02-16 17:17:32 +0100

    added some test to workflows

--- modules/gem.community/gem/community/tests/test_roles.py
+++ modules/gem.community/gem/community/tests/test_roles.py
@@ -20,12 +20,46 @@ class TestRoles(FunctionalTestCase):
         # and the manage users permission
         self.assertTrue(GROUP_ADMINISTRATOR_ROLE in [item['name'] for item in self.portal.rolesOfPermission('Manage users') if item['selected']])
 
+class TestWorkflows(FunctionalTestCase):
+    """ """
+    def afterSetUp(self):
+        FunctionalTestCase.afterSetUp(self)
+#        roles = ('Member', 'Contributor')
+#        self.portal.portal_membership.addMember('contributor',
+#                                                'secret',
+#                                                roles, [])
+
+        self.loginAsPortalOwner()
+        self.portal.invokeFactory('Document', id="testdoc")
+        self.portal.invokeFactory('File', id="testfile")
+        
+    def test_exists(self):
+        """Tests workflow existance"""
+        self.assertTrue('wiki_workflow' in self.portal.portal_workflow)
+        self.assertTrue('file_workflow' in self.portal.portal_workflow)
+
+    def test_wf_assigned(self):
+        """Tests workflow"""
+        for portal_type, chain in self.portal.portal_workflow.listChainOverrides():
+            if portal_type == 'File':            
+                self.assertTrue(('file_workflow',), chain)
+            if portal_type == 'Document':            
+                self.assertTrue(('wiki_workflow',), chain)
+
+#    def test_wiki_wf(self):
+#        """Tests workflow"""
+#        wf = self.portal.portal_workflow
+#        wf.doActionFor(self.portal.testdoc, action="")
 
 
+    def tearDown(self):
+        self.logout()
+        
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestRoles))
+    suite.addTest(makeSuite(TestWorkflows))
     return suite
 
 
