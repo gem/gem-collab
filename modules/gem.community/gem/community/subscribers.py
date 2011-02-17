0b5c704e9da31438f2f343a17b0e54751066f468
Davide Moro <davide.moro@redomino.com>
2011-02-17 13:00:21 +0100

    fix creation wiki and files area

--- modules/gem.community/gem/community/subscribers.py
+++ modules/gem.community/gem/community/subscribers.py
@@ -12,8 +12,10 @@ def initialize_community(obj, event):
             obj.invokeFactory(type_name='WikiArea', id=WIKI_AREA_ID)
             wiki = getattr(obj, WIKI_AREA_ID)
             wiki.setTitle('Wiki')
+            wiki.reindexObject()
         if FILE_AREA_ID not in obj_ids:
             obj.invokeFactory(type_name='FileArea', id=FILE_AREA_ID)
-            files = getattr(obj, WIKI_AREA_ID)
+            files = getattr(obj, FILE_AREA_ID)
             files.setTitle('Files')
+            files.reindexObject()
  
