b6d96c7f807fd5e4f87728e95f9590b0fb632e30
Maurizio Lupo <mlupo@mlupo-laptop.(none)>
2011-02-22 15:43:06 +0100

    the group administrator has the contributor and editor role inside the communities created by him

--- modules/gem.community/gem/community/subscribers.py
+++ modules/gem.community/gem/community/subscribers.py
@@ -8,17 +8,22 @@ from gem.community.config import FILE_AREA_ID
 def initialize_community(obj, event):
     """ """
     
-    if ICommunity.providedBy(obj):
-        portal_types = getToolByName(obj, 'portal_types')
-        obj_ids = obj.objectIds()
-        if WIKI_AREA_ID not in obj_ids:
-            portal_types.constructContent('WikiArea', obj, WIKI_AREA_ID)
-            wiki = getattr(obj, WIKI_AREA_ID)
-            wiki.setTitle('Wiki')
-            wiki.reindexObject()
-        if FILE_AREA_ID not in obj_ids:
-            portal_types.constructContent('FileArea', obj, FILE_AREA_ID)
-            files = getattr(obj, FILE_AREA_ID)
-            files.setTitle('Files')
-            files.reindexObject()
+    portal_types = getToolByName(obj, 'portal_types')
+    obj_ids = obj.objectIds()
+    if WIKI_AREA_ID not in obj_ids:
+        portal_types.constructContent('WikiArea', obj, WIKI_AREA_ID)
+        wiki = getattr(obj, WIKI_AREA_ID)
+        wiki.setTitle('Wiki')
+        wiki.reindexObject()
+    if FILE_AREA_ID not in obj_ids:
+        portal_types.constructContent('FileArea', obj, FILE_AREA_ID)
+        files = getattr(obj, FILE_AREA_ID)
+        files.setTitle('Files')
+        files.reindexObject()
+
+    mtool = getToolByName(obj, 'portal_membership')
+    member = mtool.getAuthenticatedMember()
+    if member:
+        obj.manage_addLocalRoles(member.getId(), [ 'Contributor', 'Editor']) 
+        obj.reindexObjectSecurity()
  
